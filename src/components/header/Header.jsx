83ad07b203459bdef82dfcb69c5070168031b59d
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-08-24 12:18:06 +0400

    Adding timing function to Header components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,35 @@
 import './Header.css';
 
+
+
 const Header = () =>
 {
+    // let currentHour, currentMinute;
+
+    
+    // const setClock = () =>
+    // {   
+    //     const hourHtml = document.querySelector('.app-date-time');
+    //     const weekDay= document.querySelector('.app-date-day');
+    //     const dayNum = document.querySelector('.app-date-num');
+    //     let weekDaysArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    //     const currentTime = new Date();
+    //     let currentMinute = currentTime.getMinutes();
+    //     let currentHour = currentTime.getHours();
+    //     let currentDay = currentTime.getDay();
+
+    //     let fullTimer = `${currentHour} : ${currentMinute}`;
+
+    //     // hourHtml.innerHTML = fullTimer;
+    //     // dayNum.innerHTML = currentDay;
+        
+    // }
+    // setInterval(() =>
+    // {
+    //     setClock();
+    // }, );
+
     return (
         <div className='app-header-wrapper'>
             <div className='app-header-date-wrapper'>
@@ -10,12 +38,37 @@ const Header = () =>
                     <span className='app-date-num'>9</span>
                 </div>
                 <div className='app-date-time-am-pm'>
-                    <span className='app-date-time'>6:23</span>
+                    <span className='app-date-time'></span>
                     <span className='app-date-am-pm'>am</span>
                 </div>
             </div>
         </div>
     )
-}
+};
+
+ const setClock = () =>
+    {   
+        const hourHtml = document.querySelector('.app-date-time');
+        const weekDay= document.querySelector('.app-date-day');
+        const dayNum = document.querySelector('.app-date-num');
+        const amPm = document.querySelector('.app-date-am-pm');
+        let weekDaysArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+        const currentTime = new Date();
+        let currentMinute = currentTime.getMinutes();
+        let currentHour = currentTime.getHours();
+        let currentDay = currentTime.getDay();
+
+        let fullTimer = `${currentHour} : ${currentMinute}`;
+
+        hourHtml.innerHTML = fullTimer;
+        dayNum.innerHTML = currentDay;
+        weekDay.innerHTML = weekDaysArray[currentDay].substring(0, 4);
+        amPm.innerHTML = (currentHour < 12) ? "AM" : "PM";
+    }
+    setInterval(() =>
+    {
+        setClock();
+    }, );
 
 export default Header;
